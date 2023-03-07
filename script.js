7fa742ee69f6b58fb14ba0ec1ce01847acb45907
ReconSempai <matiaslimina@gmail.com>
2023-03-07 12:19:42 -0300

    create operate function

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 
 function add (a,b){
-    return a+b
-}
+    return a+b;
+};
 function substract (a,b){
-    return a-b
-}
+    return a-b;
+};
 function multiply (a,b){
-    return a*b
-}
+    return a*b;
+};
 function divide (a,b){
-    return a/b
+    return a/b;
+};
+
+function operate(n1,n2,operator){
+    if (operator == '+'){
+        return add (n1,n2);
+    }
+    else if(operator == '-'){
+        return substract (n1,n2);
+    }
+    else if(operator == '*'){
+        return multiply (n1,n2);
+    }
+    else if (operator == '/'){
+        return divide (n1,n2);
+    }
 }
+
